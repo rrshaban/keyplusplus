4ec36e9f168de88e437ab9de9a680923e3372238
Razi <rrshaban@users.noreply.github.com>
2016-01-26 22:42:31 -0500

    wrote regexes for language features and beginning to explore python ast

--- README.md
+++ README.md
@@ -4,3 +4,9 @@
 
 
 Will release O'Khaled, a JIT compiler for 🔑++.
+
+#### Loops
+
+ride wit me
+  ...
+another one if [condition]
---
+++ src/fibonacci_in_python_w_ast.py
@@ -0,0 +1,36 @@
+def fibonacci(n):
+    acc1 = 0
+    acc2 = 1
+
+    while True:
+        temp = acc2
+        acc2 = acc2 + acc1
+        acc1 = temp
+        n = n - 1
+
+    if n < 1:
+        break
+
+    return acc1
+
+
+Module(
+    body=[FunctionDef(name='fibonacci', args=arguments(
+                                        args=[Name(id='n', ctx=Param())], 
+                                        vararg=None, 
+                                        kwarg=None, 
+                                        defaults=[]), 
+                    body=[
+                        Assign(targets=[Name(id='acc1', ctx=Store())], value=Num(n=0)), 
+                        Assign(targets=[Name(id='acc2', ctx=Store())], value=Num(n=1)), 
+                        While(
+                            test=Name(id='True', ctx=Load()), 
+                            body=[
+                                Assign(targets=[Name(id='temp', ctx=Store())], value=Name(id='acc2', ctx=Load())), 
+                                Assign(targets=[Name(id='acc2', ctx=Store())], value=BinOp(left=Name(id='acc2', ctx=Load()), op=Add(), right=Name(id='acc1', ctx=Load()))), 
+                                Assign(targets=[Name(id='acc1', ctx=Store())], value=Name(id='temp', ctx=Load())), 
+                                Assign(targets=[Name(id='n', ctx=Store())], value=BinOp(left=Name(id='n', ctx=Load()), op=Sub(), right=Num(n=1)))
+                                ], orelse=[]), 
+                        If(test=Compare(left=Name(id='n', ctx=Load()), ops=[Lt()], comparators=[Num(n=1)]), 
+                            body=[Break()], orelse=[]), 
+                        Return(value=Name(id='acc1', ctx=Load()))], decorator_list=[])])
